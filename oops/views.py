11ae0f4201370cd688ade649b22d85767581432d
AidenH <aidenalexh@gmail.com>
2017-12-22 15:43:32 -0500

    Add variable number of tiles with individually-modifiable contents. Fix css layout.

--- oops/views.py
+++ oops/views.py
@@ -2,5 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    tileindex = [0] * 3
+    tileindex = []
+    for i in range(1, 11):
+        tileindex.append(int(i))
+
+    # saved in case tile_container's style="width: {{ width }}%;" is needed
+    # widthint = 26 * len(tileindex)
+    # width = str(widthint) + "%"
+
     return render_to_response('index.html', locals())
