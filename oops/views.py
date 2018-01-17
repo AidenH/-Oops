18e04d3b1388cf67c98d8100c0532c13d5f1386d
AidenH <aidenalexh@gmail.com>
2018-01-17 16:47:44 -0500

    Remove unnecessary variables in views.py.

--- oops/views.py
+++ oops/views.py
@@ -4,15 +4,7 @@ from django.shortcuts import render_to_response
 from oops.models import Tile
 
 def index(request):
-    tileindex = []
-    for i in range(1, len(Tile.objects.all())+1):
-        tile_title = str(Tile.objects.filter(id=i)
-            .values_list('title', flat=True).get())
-        tile_date = re.findall(r'20..-..-.. ..:..:..', str(Tile.objects.filter(id=i)
-            .values_list('date', flat=True).get()))[0]
-        tile_content = str(Tile.objects.filter(id=i)
-            .values_list('content', flat=True).get())
-        tileindex.append(int(i))
+    tileindex = Tile.objects.all()
 
     # saved in case tile_container's style="width: {{ width }}%;" is needed
     # widthint = 26 * len(tileindex)
