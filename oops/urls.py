2e5700ddc36833e0683f902e44fda5579a71af4b
AidenH <aidenalexh@gmail.com>
2018-01-18 19:56:52 -0500

    Add url for tile update requests

--- oops/urls.py
+++ oops/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
@@ -22,4 +23,5 @@ from oops.views import *
 urlpatterns = [
     url(r'^$', index),
     url(r'^admin/', admin.site.urls),
+    url(r'^tile-update/$', tile_update),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
