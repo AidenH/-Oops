f1cda701e9a726c566bffe41e7718f932ac9819f
AidenH <aidenalexh@gmail.com>
2018-01-18 19:58:01 -0500

    Implement view function for automatic tile updating on user input

--- oops/views.py
+++ oops/views.py
@@ -1,4 +1,3 @@
-import re
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from oops.models import Tile
@@ -11,3 +10,22 @@ def index(request):
     # width = str(widthint) + "%"
 
     return render_to_response('index.html', locals())
+
+def tile_update(request):
+    def strip_content(a):
+        out = a.replace('&nbsp;', ' ').replace('<br>', '\n')
+        return out
+
+    tile_id = request.GET["id"]
+    content_updated = request.GET["content"]
+
+    if tile_id == '':
+        tile_id = 0
+
+    try:
+        print("Id: %s" % tile_id)
+        print("Content: %s" % content_updated)
+        Tile.objects.filter(id=tile_id).update(content=strip_content(content_updated))
+        return HttpResponse(status=200)
+    except:
+        return HttpResponse(status=403)
