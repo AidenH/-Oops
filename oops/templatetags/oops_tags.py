669b94e4013fce27cf21babb75c706727b1f94ce
AidenH <aidenalexh@gmail.com>
2018-01-18 19:56:23 -0500

    Create pytileUpdate (unused for the moment - may remove later)

--- oops/templatetags/oops_tags.py
+++ oops/templatetags/oops_tags.py
@@ -4,5 +4,9 @@ from oops.models import Tile
 register = template.Library()
 
 @register.simple_tag
-def pytileUpdate(tile_id, content_new):
+def pytileUpdate(content_new, tile_id):
+    if tile_id == '':
+        tile_id = 0
     Tile.objects.filter(id=tile_id).update(content=content_new)
+    print("test")
+    return 1
