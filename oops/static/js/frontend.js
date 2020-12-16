fb9acea9e838a3afd543351aef79f123098f907e
Aiden <aidenalexh@gmail.com>
2020-12-16 17:11:16 -0500

    Add mouseover functionality for individual delete buttons.

--- oops/static/js/frontend.js
+++ oops/static/js/frontend.js
@@ -1,3 +1,10 @@
+//Separate function for enlarging delete buttons. Had to add here in order
+//to pass unique tile id to function for a particular delete button.
+function deleteHover(tile_id) {
+  console.log(tile_id);
+  $('#delete'+tile_id).addClass('hover');
+}
+
 $(document).ready(function() {
 
   //Add button
@@ -9,11 +16,14 @@ $(document).ready(function() {
     $('#addbutton').removeClass('hover');
   })
 
-  //Delete button
-  $('.delete').on('mouseover', function() {
+  //Old delete button mouseover function.
+  /*$('.delete').on('mouseover', function(tile_id) {
+    console.log(tile_id);
     $('.delete').addClass('hover');
-  })
+  })*/
 
+  //Delete mouseout left the same because we don't need to use a specific
+  //delete button.
   $('.delete').on('mouseout', function() {
     $('.delete').removeClass('hover');
   })
